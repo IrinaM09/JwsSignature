084592251fec0be02f0f726cabf6dfede5da08af
Irina Mitocaru <irina.mitocaru@advahoo.ro>
2019-07-25 14:25:02 +0300

    resolve bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,11 @@
 package com.company;
 
-import sun.misc.IOUtils;
 import sun.security.util.DerInputStream;
 import sun.security.util.DerValue;
 
 import java.io.*;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.*;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.*;
 import java.util.Base64;
 
@@ -19,27 +14,32 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class Main {
 
     private static void signatureGenerator(String payload) {
-        MessageDigest digest = null;
+        MessageDigest digest;
         try {
+            /* Compute Header */
+            String header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpPU0UiLCJraWQiOiIxMzNjMTE0NzA3NDBkN2VkMzNjODZjMzUwMWUzYWM4MjIxZmVjZTAzIn0";
+
+            System.out.println("header: \n" + header);
+
+            /*Compute String for {"SHA256":"<String>"} which will be the Payload */
             digest = MessageDigest.getInstance("SHA-256");
             byte[] encodedhash = digest.digest(payload.getBytes(UTF_8));
             String result = bytesToHex(encodedhash);
 
-            System.out.println(result);
+            String part2 = "{\"SHA256\":\"" + result + "\"}";
 
-            String part2 = "{\"SHA256\":" + result + "\"}";
             System.out.println(part2);
 
-            String encodedURL = Base64.getUrlEncoder().encodeToString(part2.getBytes());
+            String encodedURL = Base64.getEncoder().encodeToString(part2.getBytes());
             encodedURL = encodedURL.replaceAll("\\+", "-");
             encodedURL = encodedURL.replaceAll("/", "_");
             encodedURL = encodedURL.replaceAll("=", "");
-            System.out.println(encodedURL);
 
-            String header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpPU0UiLCJraWQiOiIxMzNjMTE0NzA3NDBkN2VkMzNjODZjMzUwMWUzYWM4MjIxZmVjZTAzIn0";
 
+            System.out.println("payload: \n" + encodedURL);
+
+            /* Compute signature */
             String signatureConcatenation = header + "." + encodedURL;
-            System.out.println(signatureConcatenation);
 
             File file = new File("cert.key");
 
@@ -50,8 +50,6 @@ public class Main {
 
             while ((i = br.read()) != -1)
                 privateKeyContent += (char) i;
-
-            System.out.println(privateKeyContent);
 
             privateKeyContent = privateKeyContent.replace("-----BEGIN RSA PRIVATE KEY-----", "");
             privateKeyContent = privateKeyContent.replace("-----END RSA PRIVATE KEY-----", "");
@@ -76,44 +74,27 @@ public class Main {
             RSAPrivateCrtKeySpec keySpec = new RSAPrivateCrtKeySpec(modulus, publicExp, privateExp, prime1, prime2, exp1, exp2, crtCoef);
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             PrivateKey pKey = keyFactory.generatePrivate(keySpec);
-            System.out.println(pKey);
 
-            Signature sign = Signature.getInstance("SHA1withRSA");
+            Signature sign = Signature.getInstance("SHA256withRSA");
             sign.initSign(pKey);
-            sign.update(signatureConcatenation.getBytes("UTF-8"));
+            sign.update(signatureConcatenation.getBytes(UTF_8));
             String signedSignature = new String(Base64.getEncoder().encode(sign.sign()));
-
-            System.out.println(signedSignature);
 
             signedSignature = signedSignature.replaceAll("\\+", "-");
             signedSignature = signedSignature.replaceAll("/", "_");
             signedSignature = signedSignature.replaceAll("=", "");
 
-            System.out.println("result: ");
+            System.out.println("JWS-signature: ");
             String signature = signatureConcatenation + "." + signedSignature;
-            System.out.printf(signature);
+            System.out.println(signature);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-
-    public static PrivateKey get(String filename)
-            throws Exception {
-
-        byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
-
-        PKCS8EncodedKeySpec spec =
-                new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        return kf.generatePrivate(spec);
-    }
-
 
     private static String bytesToHex(byte[] hash) {
-        StringBuffer hexString = new StringBuffer();
+        StringBuilder hexString = new StringBuilder();
         for (int i = 0; i < hash.length; i++) {
             String hex = Integer.toHexString(0xff & hash[i]);
             if (hex.length() == 1) hexString.append('0');
@@ -123,7 +104,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        //PLEASE INSERT JSON PAYLOAD BELOW
         String payload = "{\"headers\":{\"Branch-Location\":\"RO\",\"X-Request-ID\":\"b7d96357-b320-4f54-bb02-1c4511e4b772\",\"PSU-IP-Address\":\"127.0.0.1\"},\"payload\":{\"endToEndIdentification\":\"test\",\"instructedAmount\":{\"currency\":\"RON\",\"amount\":\"101\"},\"creditorAccount\":{\"iban\":\"RO61TREZ27A660404200109X\"},\"creditorName\":\"PaySafe\"}}";
+       // payload = "{\"headers\":{\"Branch-Location\":\"RO\",\"X-Request-ID\":\"35ffcd8e-3453-4dcc-a6cc-c74ea2344822\"},\"payload\":{\"access\":{\"balances\":[],\"transactions\":[]},\"recurringIndicator\":true,\"validUntil\":\"2019-11-01\",\"frequencyPerDay\":\"4\"}}";
         signatureGenerator(payload);
     }
 }
